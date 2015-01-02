579e6b6cd8841dd91dbddfa570430a35fb141f0a
wiseman <jjwiseman@gmail.com>
2015-01-01 20:27:35 -0800

    Version 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lemondronor/turboshrimp-xuggler "0.0.1-SNAPSHOT"
+(defproject com.lemondronor/turboshrimp-xuggler "0.0.1"
   :description (str "An AR.Drone video decoder for the turboshrimp library "
                     "that uses the xuggler H.264 decoder.")
   :url "https://github.com/wiseman/turboshrimp-xuggler"
